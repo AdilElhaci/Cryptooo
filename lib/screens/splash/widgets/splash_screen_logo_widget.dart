37527677a5c58c62faab6e734876588b8ba8dbd4
furkanksl <furkankoseoglu65@gmail.com>
2021-03-29 20:10:57 +0300

    created splash screen

---
+++ lib/screens/splash/widgets/splash_screen_logo_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+Widget logoContainer() {
+  return SvgPicture.asset(
+    'assets/images/logo.svg',
+    height: 153,
+    width: 153,
+  );
+}
