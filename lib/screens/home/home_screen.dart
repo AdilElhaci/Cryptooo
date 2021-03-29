7e0b44bb35cc0d9a92798eb70a5ec4c44f242fc8
AdilElhaci <adelalhajji97@gmail.com>
2021-03-30 00:36:53 +0300

    splash screen is done

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeSecreen extends StatefulWidget {
+  @override
+  _HomeSecreenState createState() => _HomeSecreenState();
+}
+
+class _HomeSecreenState extends State<HomeSecreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,5 +1,7 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:cryptoo/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'widgets/splash_screen_logo_widget.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,24 +14,21 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return AnimatedSplashScreen(
       backgroundColor: Color(0xFF129898),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Cryptooo',
-              style: TextStyle(
-                fontSize: 48,
-              ),
-            ),
-            SizedBox(height: 30),
-            logoContainer()
-          ],
-        ),
+      splash: Column(
+        children: [
+          Text(
+            'Cryptooo',
+            style: GoogleFonts.spaceMono(textStyle: TextStyle(fontSize: 48)),
+          ),
+          SizedBox(height: 30),
+          logoContainer()
+        ],
       ),
+      nextScreen: HomeSecreen(),
+      splashIconSize: 300,
+      splashTransition: SplashTransition.sizeTransition,
     );
   }
 }
