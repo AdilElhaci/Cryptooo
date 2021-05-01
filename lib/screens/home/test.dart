6be40aa51d9489168a74ea32bf8235512db4819b
AdilElhaci <adelalhajji97@gmail.com>
2021-05-02 02:44:41 +0300

    sqflite version 1

---
+++ lib/screens/home/test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestScreeen extends StatefulWidget {
+  TestScreeen({Key key}) : super(key: key);
+
+  @override
+  _TestScreeenState createState() => _TestScreeenState();
+}
+
+class _TestScreeenState extends State<TestScreeen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
