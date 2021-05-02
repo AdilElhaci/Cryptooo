83cc23a82318c2138f37c2679047c59536433e76
AdilElhaci <adelalhajji97@gmail.com>
2021-05-03 00:50:13 +0300

    changes on sat

--- lib/core/constants/icons_url.dart
+++
@@ -1,5 +0,0 @@
-class IconURL {
-  static const INCREASEICON = 'assets/icons/increaseicon.svg';
-  static const DECREASEICON = 'assets/icons/decreaseicon.svg';
-  static const GOBACK = 'assets/icons/goback.svg';
-}
--- lib/core/constants/theme.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class ProjectThemes {
-  static const Color MAINCOLOR = Color(0xFF129898);
-}
