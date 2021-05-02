9318b85c0aec997c59345337b73af647fc4a84a1
AdilElhaci <adelalhajji97@gmail.com>
2021-05-03 00:51:45 +0300

    fixed wrongs

---
+++ lib/screens/cryptoanalysis/widgets/crypto_analysis_screen_analysis.dart
@@ -0,0 +1,60 @@
+import 'package:cryptoo/core/models/crypto.model.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class LineSceeen extends StatefulWidget {
+  final CryptoModel cryptoModel;
+
+  const LineSceeen({Key key, this.cryptoModel}) : super(key: key);
+  @override
+  _LineSceeenState createState() => _LineSceeenState();
+}
+
+class _LineSceeenState extends State<LineSceeen> {
+  List<PriceData> _chartList;
+  TooltipBehavior _tooltipBehavior;
+  @override
+  void initState() {
+    super.initState();
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    _chartList = getPriceData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(widget.cryptoModel.priceUsd);
+    return Container(
+      child: SfCartesianChart(
+        title: ChartTitle(text: 'Weekly crypto prices'),
+        tooltipBehavior: _tooltipBehavior,
+        series: <ChartSeries>[
+          LineSeries<PriceData, double>(
+              dataSource: _chartList,
+              xValueMapper: (PriceData price, _) => price.year,
+              yValueMapper: (PriceData price, _) => price.price,
+              dataLabelSettings: DataLabelSettings(isVisible: true),
+              enableTooltip: true)
+        ],
+        primaryXAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
+        primaryYAxis: NumericAxis(
+            numberFormat: NumberFormat.simpleCurrency(decimalDigits: 0)),
+      ),
+    );
+  }
+
+  List<PriceData> getPriceData() {
+    final List<PriceData> chartData = [
+      PriceData(2019, 2000),
+      PriceData(2020, 18200),
+      PriceData(2021, widget.cryptoModel.priceUsd),
+    ];
+    return chartData;
+  }
+}
+
+class PriceData {
+  final double year;
+  final double price;
+  PriceData(this.year, this.price);
+}
