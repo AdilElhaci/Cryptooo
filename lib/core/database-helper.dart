33f50c0aaf078667a6bea8705687bd57ec512c24
AdilElhaci <adelalhajji97@gmail.com>
2021-05-03 18:22:41 +0300

    db version1

--- lib/core/database-helper.dart
+++ lib/core/database-helper.dart
@@ -1,13 +1,21 @@
+// import 'dart:io';
+
 // import 'package:sqflite/sqflite.dart';
 // import 'models/weekly-crpyto.model.dart';
+// import 'dart:async';
+// import 'package:path_provider/path_provider.dart';
+// import 'package:path/path.dart';
 
 // class DatabaseHelper {
-//   static DatabaseHelper _databaseHelper;
+//   static const String DATABASE_NAME = "crypto.db";
+//   static const int DATABASE_VERSION = 1;
 
-//   static Database _database;
+//   static Database _db;
 
-//   static final _databaseVersion = 1;
-//   static final _databaseName = 'crypto';
+//   static final DatabaseHelper _instance = new DatabaseHelper.internal();
+//   DatabaseHelper.internal();
+//   factory DatabaseHelper() => _instance;
+
 //   static final _tableName = 'weekly_prices';
 //   static final _columnId = 'id';
 //   static final _columnSymbol = 'symbol';
@@ -20,82 +28,53 @@
 //   static final _columnSat = 'sat';
 //   static final _columnSun = 'sun';
 
-// //for Singleton
-//   DatabaseHelper._internal();
-
-//   factory DatabaseHelper() {
-//     if (_databaseHelper == null) {
-//       //   return _databaseHelper = DatabaseHelper._internal();
-//     } else {
-//       return _databaseHelper;
+//   Future<Database> get database async {
+//     if (_db != null) {
+//       return _db;
 //     }
-//   }
 
-//   Future<Database> getDatabase() async {
-//     _database ??= await _initialDatabase();
-//     if (_database != null) {
-//       _database = await _initialDatabase();
-//     }
-//     return _database;
+//     _db = await initDb();
+//     return _db;
 //   }
 
 //   // this opens the database (and creates it if it doesn't exist)
-//   Future<Database> _initialDatabase() async {
-//     // Directory directory = await getApplicationDocumentsDirectory();
 
-//     var txDB = await openDatabase(_databaseName,
-//         version: _databaseVersion, onCreate: _createDB);
-//     return txDB;
-//   }
-
-//   Future<void> insert(WeeklyCryptoModel model) async {
-//     if (_database != null) {
-//       _database = await _initialDatabase();
-//     }
-//     return await _database.insert(_tableName, model.toJson());
+//   initDb() async {
+//     Directory documentsDirectory = await getApplicationDocumentsDirectory();
+//     String path = join(documentsDirectory.path, DATABASE_NAME);
+//     return await openDatabase(path, version: DATABASE_VERSION, onCreate: _createDB);
 //   }
 
 //   // SQL code to create the database table
 //   Future _createDB(Database db, int version) async {
-//     var queryWeeklyCrypto = '''
-//         CREATE TABLE $_tableName (
-//         $_columnId INTEGER PRIMARY KEY AUTOINCREMENT,
-//         $_columnSymbol TEXT NOT NULL,
-//         $_columnDate TEXT NOT NULL,
-//         $_columnMon REAL NOT NULL,
-//         $_columnTues REAL NOT NULL,
-//         $_columnWed REAL NOT NULL,
-//         $_columnThur REAL NOT NULL,
-//         $_columnFrid REAL NOT NULL,
-//         $_columnSat REAL NOT NULL,
-//         $_columnSun REAL NOT NULL,
-//         );
-//     ''';
+//     var queryWeeklyCrypto =
+//         'CREATE TABLE $_tableName ($_columnId INTEGER PRIMARY KEY AUTOINCREMENT,$_columnSymbol TEXT NOT NULL,$_columnDate TEXT NOT NULL,$_columnMon REAL NOT NULL,$_columnTues REAL NOT NULL,$_columnWed REAL NOT NULL,$_columnThur REAL NOT NULL,$_columnFrid REAL NOT NULL,$_columnSat REAL NOT NULL,$_columnSun REAL NOT NULL)';
 //     await db.execute(queryWeeklyCrypto);
 //   }
 
-//   Future<int> addCrypto(WeeklyCryptoModel item) async {
-//     var db = await getDatabase();
-//     var result = db.insert(_tableName, item.toJson());
-//     return result;
-//   }
-
 //   Future<List<Map<String, dynamic>>> allData() async {
-//     var db = await getDatabase();
+//     var db = _db;
 //     var result = db.query(_tableName, orderBy: '$_columnId DESC');
 //     return result;
 //   }
 
+//   Future<int> insertCrypto(WeeklyCryptoModel item) async {
+//     if (_db == null) {
+//       _db = await initDb();
+//     }
+//     var db = _db;
+//     var result = db.insert(_tableName, item.toJson());
+//     return result;
+//   }
+
 //   Future<int> updateCrypto(WeeklyCryptoModel item) async {
-//     var db = await getDatabase();
-//     var result = db.update(_tableName, item.toJson(),
-//         where: '$_columnId = ? ', whereArgs: [item.id]);
+//     var db = _db;
+//     var result = db.update(_tableName, item.toJson(), where: '$_columnId = ? ', whereArgs: [item.id]);
 //     return result;
 //   }
 
 //   Future<int> deleteCrypto(int id) async {
-//     var db = await getDatabase();
-//     return await db
-//         .delete(_tableName, where: '$_columnId = ?', whereArgs: [id]);
+//     var db = _db;
+//     return await db.delete(_tableName, where: '$_columnId = ?', whereArgs: [id]);
 //   }
 // }
